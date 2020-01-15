e33c15a5252f386061d108153d8307c5b9a452ae
artsiom-tsaryonau <artemii1992@gmail.com>
2020-01-15 15:41:50 +0100

    Section 2.2

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,15 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x = 42
+  println(x)
+
+  val aString: String = "hello"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val aShort: Short = 23
+
+  var aVariable: Int = 4
+  aVariable = 5
+}
