0eebd7ab167bbb9615007ef6db4e2d90bb32719e
artsiom-tsaryonau <artemii1992@gmail.com>
2020-01-16 11:42:47 +0100

    Section 3.11

---
+++ src/lectures/part2oop/OOBasics.scala
@@ -0,0 +1,70 @@
+package lectures.part2oop
+
+object OOBasics extends App {
+
+  val person = new Person("John", 26)
+  println(person.x)
+  person.greet("Daniel")
+  person.greet()
+
+  val author = new Writer("Charles", "Dickens", 1812)
+  val impostor = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expectations", 1861, author)
+  println(novel.authorAge)
+  println(novel.isWrittenBy(impostor))
+
+  val counter = new Counter
+  counter.inc.print
+  counter.inc.inc.inc.print
+  counter.inc(10).print
+}
+
+// constructor
+class Person(name: String, val age: Int) {
+  // body
+  val x = 2
+
+  println(1 + 3)
+
+  def greet(name: String): Unit = println(s"${this.name} say: Hi, $name")
+
+  def greet(): Unit = println(s"High, I am $name")
+
+  def this(name: String) = this(name, 0)
+}
+
+// class parameters are not field
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullName: String = firstName + " " + surname
+}
+
+class Novel(name: String, year: Int, author: Writer) {
+  def authorAge = year - author.year
+  def isWrittenBy(author: Writer) = author == this.author
+  def copy(newYear: Int) = new Novel(name, newYear, author)
+}
+
+class Counter(val count: Int = 0) {
+  def inc = {
+    println("incrementing")
+    new Counter(count + 1)
+  }
+
+  def dec = {
+    println("decrementing")
+    new Counter(count - 1)
+  }
+
+  def inc(n: Int): Counter = {
+    if (n <= 0) this
+    else inc.inc(n - 1)
+  }
+
+  def dec(n: Int): Counter = {
+    if (n <= 0) this
+    else dec.dec(n - 1)
+  }
+
+  def print = println(count)
+}
